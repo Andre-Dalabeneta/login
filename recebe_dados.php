32187b7a7363575968b4c4c0f135325304825597
Andre-Dalabeneta <50251526+Andre-Dalabeneta@users.noreply.github.com>
2019-11-18 22:48:54 -0200

    acabou aula11
    acabou a aula

--- recebe_dados.php
+++ recebe_dados.php
@@ -93,10 +93,25 @@ if (isset($_POST['action'])) {
         }
     } else if ($_POST['action'] == 'senha') {
         //Senão, teste se ação é recuperar senha
-        echo "\n<p>senha</p>";
-        echo "\n<pre>";
-        print_r($_POST);
-        echo "\n<pre>";
+        $email = verificar_entrada($_POST['emailGerarSenha']);
+        $sql = $connect->prepare("SELECT idUsuario FROM usuario WHERE emailUsuario = ?");
+        $sql->bind_param("s", $email);
+        $sql->execute();
+        $resposta = $sql->get_result();
+        if($resposta->num_rows > 0){
+            //echo "E-mail encontrado!";
+            $frase = "escreviESaiCorrendoPauNoCuDeQuemTaLendo123";
+            $palavra_secreta = str_shuffle($frase);
+            $token = substr($palavra_secreta,0,10);
+            //echo "Token: $token";
+            $sql = $connect->prepare("UPDATE usuario SET token=?, tempoDeVida=?DATE_ADD(now(), INTERVAL 1 MINUTE)WHERE emailUsusario = ?");
+            $sql->bind_param("ss", $token, $email);
+            $sql->execute();
+            echo "Token no banco de dados";
+        }
+        else{
+            echo "E-mail não encontrado!";
+        }
     } else {
         header("location:index.php");
     }
